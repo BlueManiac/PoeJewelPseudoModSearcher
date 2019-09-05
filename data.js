b3c34a5d45a886d7d608ffcbf72026769f969b93
BlueManiac <bluemaniac@users.noreply.github.com>
2019-09-05 22:07:15 +0200

    Improve startup by preloading assets.

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-let statsPromise = axios.get("./data/trade_stats.json").then(x => x.data.result.find(x => x.label == "Explicit").entries);
-let leaugesPromise = axios.get("./data/leagues.json").then(x => x.data.result);
+let statsPromise = fetch("./data/trade_stats.json").then(x => x.json()).then(x => x.result.find(x => x.label == "Explicit").entries);
+let leaugesPromise = fetch("./data/leagues.json").then(x => x.json()).then(x => x.result);
 
 export { statsPromise, leaugesPromise };

2c1c2337777b375928fd27bd067897d39b06c3ef
BlueManiac <bluemaniac@users.noreply.github.com>
2019-12-13 20:34:54 +0100

    Updates for 3.9

--- data/download.ps1
+++ data/download.ps1
@@ -2,8 +2,8 @@ Invoke-WebRequest -Uri "https://www.pathofexile.com/api/trade/data/stats" -OutFi
 
 Invoke-WebRequest -Uri "https://www.pathofexile.com/api/trade/data/leagues" -OutFile "leagues.json"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/stat_translations.json" -OutFile "stat_translations.json"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/mods.json" -OutFile "mods.json"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/stats.json" -OutFile "stats.json"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/mod_types.json" -OutFile "mod_types.json"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/data/tags.json" -OutFile "tags.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/stat_translations.json" -OutFile "stat_translations.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/mods.json" -OutFile "mods.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/stats.json" -OutFile "stats.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/mod_types.json" -OutFile "mod_types.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/brather1ng/RePoE/master/RePoE/data/tags.json" -OutFile "tags.json"
